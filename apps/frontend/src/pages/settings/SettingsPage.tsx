568eb9b9ba18a5a07bbbda6a50ca8fe7ebcf493d
Vinicius Teruel <vpteruel@gmail.com>
2025-12-05 23:32:27 -0500

    feat: add SettingsPage component for system configuration

---
+++ apps/frontend/src/pages/settings/SettingsPage.tsx
@@ -0,0 +1,15 @@
+import "../pages.css";
+
+export function SettingsPage() {
+  return (
+    <div className="list-page">
+      <div className="page-header">
+        <h1>Settings</h1>
+        <p>Configure system settings</p>
+      </div>
+      <div className="list-container">
+        {/* Settings content will go here */}
+      </div>
+    </div>
+  );
+}
