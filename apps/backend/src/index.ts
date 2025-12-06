c7ef50d1d972a43c89b4d22f95fa6a7847fd0cee
Vinicius Teruel <vpteruel@gmail.com>
2025-12-05 22:47:27 -0500

    fix: correct import statements formatting in index.ts

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,9 +1,9 @@
+import { cors } from "@elysiajs/cors";
+import type { CreateUserDto, User } from "@shared/lib";
 import { Elysia } from "elysia";
-import { cors } from '@elysiajs/cors'
-import type { User, CreateUserDto } from '@shared/lib'
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(5000);
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`,
 );
